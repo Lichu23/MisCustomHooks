ec335f9cdfca52c4d384ed63f5c5da7c22065fa2
Lichu <lisandroxarenax@gmail.com>
2023-03-10 12:17:59 -0300

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const useCounter = (initialstate = 0) => {
+  const [counter, setCounter] = useState(initialstate);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+  const reset = () => {
+    setCounter(initialstate);
+  };
+
+  return {
+    counter,
+    increment,
+    decrement,
+    reset,
+  };
+};
